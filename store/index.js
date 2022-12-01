ca85d38793c33e96bca5096e3cd798604c31b622
rybaier <rybaier@gmail.com>
2022-12-01 13:29:58 -0500

    redux persist v4 used in course, followed course direction, consult documentation for better use of redux-persist v6!

--- store/index.js
+++ store/index.js
@@ -1,15 +1,22 @@
 import { compose, applyMiddleware } from 'redux'
 import { legacy_createStore as createStore } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, autoRehydrate } from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import localStorage from 'redux-persist/lib/storage'
 import thunk from 'redux-thunk'
 import reducers from '../reducers';
-
+//redux-persist v4 being used! redux-persist v6 has more steps involved for persisting the store.
+//persistStore error is from deprecation 
+// use documentation, persistConfig persistReducer etc.
 const store = createStore(
     reducers, 
     {},
     compose(
-        applyMiddleware(thunk)
+        applyMiddleware(thunk),
+        autoRehydrate()
     )
 )
 
+persistStore(store, { storage: AsyncStorage, whitelist: ['likedJobs']})
 export default store;
